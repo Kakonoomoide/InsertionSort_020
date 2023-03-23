7719be48ab5cc8a7051fc295786181871e54431a
kizaru <rizkiramada128@gmail.com>
2023-03-23 10:49:38 +0700

    membuat fungsi insertionsort

--- iInsertionSort.cpp
+++ iInsertionSort.cpp
@@ -8,7 +8,7 @@ int n;
 void  input() {
 
     while (true) {                                              //looping
-        cout << "masukan elemen array : \n";
+        cout << "masukan elemen/jumlah data array : \n";
         cin >> n;
         if (n <= 20)                                            // n <= 20
             break;                                              // keluar looping
@@ -27,6 +27,20 @@ void  input() {
         cin >> arr[i];
     }
 
+}
+
+void insertionsort() {
+    int i, key, j;
+    for (i = 1; i < n; i++){
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 int main() {
